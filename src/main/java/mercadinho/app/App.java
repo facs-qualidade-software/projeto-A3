e1feb0d7f5c7f5b7a2949e9e167b9285c95d0307
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2024-04-29 17:02:20 -0300

    chore: removed garbage code from main

--- src/main/java/mercadinho/app/App.java
+++ src/main/java/mercadinho/app/App.java
@@ -2,12 +2,9 @@ package mercadinho.app;
 
 import java.sql.SQLException;
 import java.sql.Connection;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class App {
-  public static void main(String[] args) throws ParseException {
+  public static void main(String[] args) {
 
     try (Connection connection = DbConexao.conectar()) {
       System.out.println("Conectado ao Banco de Dados com sucesso!");
