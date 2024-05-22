5e7d603c09faa75030b0f356e3bc0101b5a831e6
Marcelo Cavalcanti <19808040+mmcav@users.noreply.github.com>
2024-05-22 19:24:08 -0300

    Update ftpupload.sh

--- .github/ftpupload.sh
+++ .github/ftpupload.sh
@@ -3,19 +3,26 @@
 HOST=$1
 USUARIO=$2
 SENHA=$3
+DATAHORARIO=$4
 
 PASTAPROJETO="facs-qualidade-software-projetoA3"
-DATAHORARIO=$(date '+%F__%H-%M-%S')
 NOMEPASTAJACOCO="relatorio_jacoco__${DATAHORARIO}"
-ARQUIVOORIGEM="./target/site/jacoco/jacoco.xml"
-ARQUIVODESTINO="jacoco.xml"
+PASTAJACOCORESOURCES="jacoco-resources"
+PASTAMERCADINHOAPP="mercadinho.app"
 
 ftp -inv $HOST <<END_SCRIPT
 user $USUARIO $SENHA
 cd $PASTAPROJETO
 mkdir $NOMEPASTAJACOCO
 cd $NOMEPASTAJACOCO
-put $ARQUIVOORIGEM $ARQUIVODESTINO
+mput index.html jacoco.csv jacoco.xml jacoco-sessions.html
+mkdir $PASTAJACOCORESOURCES
+cd $PASTAJACOCORESOURCES
+mput $PASTAJACOCORESOURCES/*
+cd ..
+mkdir $PASTAMERCADINHOAPP
+cd $PASTAMERCADINHOAPP
+mput $PASTAMERCADINHOAPP/*
 ls
 quit
 END_SCRIPT
