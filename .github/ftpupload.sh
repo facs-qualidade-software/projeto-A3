216e0939f318022131ab0ff0defcc04a679691d1
Marcelo Cavalcanti <19808040+mmcav@users.noreply.github.com>
2024-05-22 13:36:49 -0300

    test: shell script fix attempt

--- .github/ftpupload.sh
+++ .github/ftpupload.sh
@@ -7,15 +7,15 @@ SENHA=$3
 PASTAPROJETO="facs-qualidade-software-projetoA3"
 DATAHORARIO=$(date '+%F__%H-%M-%S')
 NOMEPASTAJACOCO="relatorio_jacoco__parcial__${DATAHORARIO}"
-ARQUIVOXML="./target/site/jacoco/jacoco.xml"
+ARQUIVOORIGEM="./target/site/jacoco/jacoco.xml"
+ARQUIVODESTINO="jacoco.xml"
 
 ftp -inv $HOST <<END_SCRIPT
 user $USUARIO $SENHA
 cd $PASTAPROJETO
-passive
 mkdir $NOMEPASTAJACOCO
 cd $NOMEPASTAJACOCO
-put $ARQUIVOXML
+put $ARQUIVOORIGEM $ARQUIVODESTINO
 ls
 quit
 END_SCRIPT
